32f31dccf09b3814bb7a3d696f4adb1d999546d9
ibnakhal <inakhal2@180-705-01.students.aac.academyart.edu>
2016-02-04 23:20:03 -0800

    updated spaceship, added sounds

---
+++ Assets/NoiseMaker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoiseMaker : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip sound;
+    [SerializeField]
+    private AudioSource source;
+    // Use this for initialization
+    void Start()
+    {
+        source = this.gameObject.GetComponent<AudioSource>();
+        source.clip = sound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        source.Play();
+
+    }
+
+    public void OnTriggerEnter(Collider Other)
+    {
+        source.Play();
+
+    }
+
+}
---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour {
+    [SerializeField]
+    private Transform pointA;
+    [SerializeField]
+    private Transform pointB;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float turnSpeed;
+    [SerializeField]
+    private Vector3 newVector;
+    [SerializeField]
+    private Vector3 turnVector;
+    [SerializeField]
+    private Vector3 vectorA;
+    [SerializeField]
+    private Vector3 vectorB;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+
+    {
+        this.transform.Translate(newVector * Time.deltaTime * speed);
+        this.transform.Rotate(turnVector * Time.deltaTime * turnSpeed);
+	}
+
+    public void OnTriggerEnter(Collider Other)
+    {
+        Debug.Log("Boom");
+        if(Other.gameObject == pointA.gameObject)
+        {
+            newVector = vectorA;
+        }
+        if (Other.gameObject == pointB.gameObject)
+        {
+            newVector = vectorB;
+
+        }
+    }
+
+
+
+
+
+}
